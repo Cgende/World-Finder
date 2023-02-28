ef6b4ff602f73c89e2d7507d43ceaa3523a8c3dd
Cgende <conradgende@gmail.com>
2023-02-27 22:26:22 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # World Finder
-A world hopper which sorts by last hopped-to world
+A world hopper which adds a column to sort by when a world was last visited. This is useful for to avoid hopping to 
+worlds you've already checked when looking for a free world. Also works with the party plugin to show when
+anyone in the party was last in the world.
+
+![img.png](img.png)
--- src/main/java/com/worldfinder/WorldFinder/WorldFinderConfig.java
+++ src/main/java/com/worldfinder/WorldFinder/WorldFinderConfig.java
@@ -1,19 +1,147 @@
 package com.worldfinder.WorldFinder;
 
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import java.util.Collections;
+import java.util.Set;
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
+import net.runelite.client.config.Keybind;
 
 @ConfigGroup("worldfinder")
 public interface WorldFinderConfig extends Config
 {
+	String GROUP = "worldfinder";
+
 	@ConfigItem(
-		keyName = "greeting",
-		name = "Welcome Greeting",
-		description = "The message to show to the user when they login"
+		keyName = "previousKey",
+		name = "Quick-hop previous",
+		description = "When you press this key you'll hop to the previous world",
+		position = 0
 	)
-	default String greeting()
+	default Keybind previousKey()
 	{
-		return "Hello";
+		return new Keybind(KeyEvent.VK_LEFT, InputEvent.CTRL_DOWN_MASK | InputEvent.SHIFT_DOWN_MASK);
+	}
+
+	@ConfigItem(
+		keyName = "nextKey",
+		name = "Quick-hop next",
+		description = "When you press this key you'll hop to the next world",
+		position = 1
+	)
+	default Keybind nextKey()
+	{
+		return new Keybind(KeyEvent.VK_RIGHT, InputEvent.CTRL_DOWN_MASK | InputEvent.SHIFT_DOWN_MASK);
+	}
+
+	@ConfigItem(
+		keyName = "quickhopOutOfDanger",
+		name = "Quick-hop out of dangerous worlds",
+		description = "Don't hop to a PVP/high risk world when quick-hopping",
+		position = 2
+	)
+	default boolean quickhopOutOfDanger()
+	{
+		return true;
+	}
+
+	@ConfigItem(
+		keyName = "showSidebar",
+		name = "Show world switcher sidebar",
+		description = "Show sidebar containing all worlds that mimics in-game interface",
+		position = 4
+	)
+	default boolean showSidebar()
+	{
+		return true;
+	}
+
+	@ConfigItem(
+		keyName = "ping",
+		name = "Show world ping",
+		description = "Shows ping to each game world",
+		position = 5
+	)
+	default boolean ping()
+	{
+		return true;
+	}
+
+	@ConfigItem(
+		keyName = "showMessage",
+		name = "Show world hop message in chat",
+		description = "Shows what world is being hopped to in the chat",
+		position = 6
+	)
+	default boolean showWorldHopMessage()
+	{
+		return true;
+	}
+
+	@ConfigItem(
+		keyName = "menuOption",
+		name = "Show Hop-to menu option",
+		description = "Adds Hop-to menu option to the friends list and friends chat members list",
+		position = 7
+	)
+	default boolean menuOption()
+	{
+		return true;
+	}
+
+	@ConfigItem(
+		keyName = "subscriptionFilter",
+		name = "Subscription filter",
+		description = "Only show free worlds, member worlds, or both types of worlds in sidebar",
+		position = 8
+	)
+	default SubscriptionFilterMode subscriptionFilter()
+	{
+		return SubscriptionFilterMode.BOTH;
+	}
+
+	@ConfigItem(
+		keyName = "regionFilter",
+		name = "Region filter",
+		description = "Only show and quick-hop to worlds in specific regions",
+		position = 9
+	)
+	default Set<RegionFilterMode> regionFilter()
+	{
+		return Collections.emptySet();
+	}
+
+	@ConfigItem(
+		keyName = "worldTypeFilter",
+		name = "World type filter",
+		description = "Only show worlds of specific types",
+		position = 10
+	)
+	default Set<WorldTypeFilter> worldTypeFilter()
+	{
+		return Collections.emptySet();
+	}
+
+	@ConfigItem(
+		keyName = "displayPing",
+		name = "Display current ping",
+		description = "Displays ping to current game world",
+		position = 11
+	)
+	default boolean displayPing()
+	{
+		return false;
+	}
+	@ConfigItem(
+		keyName = "twelveHourTime",
+		name = "12 Hour Time",
+		description = "Show time in 12 hour format.",
+		position = 12
+	)
+	default boolean twelveHourTime()
+	{
+		return false;
 	}
 }
